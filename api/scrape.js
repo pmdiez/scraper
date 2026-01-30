95cfe17a6331ed81128767ee67dcd3a235050008
pmdiez <34581671+pmdiez@users.noreply.github.com>
2026-01-30 11:19:20 +0100

    Update scrape.js

--- api/scrape.js
+++ api/scrape.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio');
 export default async function handler(req, res) {
     // URL base sin el número de página
     const baseUrl = 'https://www.efectoled.com/es/11043-comprar-catalogo-decoracion?page=1';
-    const totalPagesToScan = 50; // Forzamos el escaneo de 50 páginas
+    const totalPagesToScan = 5; // Forzamos el escaneo de 50 páginas
     const allProducts = [];
 
     try {
@@ -99,6 +99,7 @@ async function fetchPage(url) {
         return []; // Si la página no existe (ej. pág 18 de 17), devolvemos array vacío
     }
 }
+
 
 
 
